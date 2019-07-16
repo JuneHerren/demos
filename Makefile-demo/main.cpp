179a6afb027c345ad64790896442b5f724761b29
JuneHerren <37968051+JuneHerren@users.noreply.github.com>
2019-07-16 15:19:19 +0800

    Add files via upload

---
+++ Makefile-demo/main.cpp
@@ -0,0 +1,19 @@
+#include"demo.hpp"
+#include<iostream>
+
+using namespace std;
+ int main(){
+     int n;
+     cin >> n;
+     vector<int> vec(n);
+     for(int i = 0; i < n; i++){
+         cin >> vec[i];
+     }
+     int target;
+     cin >> target;
+     Solution solution;
+     auto it = solution.twoSum(vec,target);
+     for(auto c: it) cout << c <<" ";
+     cout << endl;
+     return 0;
+ }
