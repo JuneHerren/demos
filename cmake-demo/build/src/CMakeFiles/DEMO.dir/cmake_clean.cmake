70c48225f61cff8ae6c8b286c44a8f3fc620194f
juneherren <1596180809@qq.com>
2019-07-16 14:28:55 +0800

    upload cmake-demo

---
+++ cmake-demo/build/src/CMakeFiles/DEMO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DEMO.dir/demo.cpp.o"
+  "libDEMO.pdb"
+  "libDEMO.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
