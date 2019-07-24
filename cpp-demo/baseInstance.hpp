08047aa7c6da6b909c80447aaa674873f76f53df
JuneHerren <37968051+JuneHerren@users.noreply.github.com>
2019-07-25 06:47:42 +0800

    Add files via upload

---
+++ cpp-demo/baseInstance.hpp
@@ -0,0 +1,13 @@
+class Singleton
+{
+private:
+	Singleton() { };
+	~Singleton() { };
+	Singleton(const Singleton&);
+	Singleton& operator=(const Singleton&);
+public:
+	static Singleton& getInstance() {
+		static Singleton instance;
+		return instance;
+	}
+};
