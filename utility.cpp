06d1947fcc881e8c8eedc80630455c87fce382d1
JuneHerren <37968051+JuneHerren@users.noreply.github.com>
2019-08-21 09:58:32 +0800

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -11,3 +11,15 @@ vector<T> split(const string& str, char delim){
 	}
 	return ans;
 }
+//拓展replace的功能
+void  replace(string &str, const string& source, const string& target){
+	auto length = str.size();
+	vector<string> cache(length);
+	for(int i = 0; i < length; i++){
+		cache[i] = str[i];
+	}
+	replace(cache.begin(),cache.end(), source, target);
+	string ans;
+	for(const auto &c: cache) ans.append(c);
+	str = move(ans);
+}
